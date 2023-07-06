0b13c60f7262e5e30d15455ff5478e8d171b1959
langochai <langochai11121998@gmail.com>
2023-07-06 09:41:56 +0700

    1st commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Welcome from "./components/Welcome.jsx";
+
+function App() {
+    return (
+        <div className="App">
+            <Welcome name="Admin" />
+        </div>
+    );
+}
+export default App;
